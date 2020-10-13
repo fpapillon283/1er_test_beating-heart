4cf14304b43c0a80988739e2e8100f3e13504f1a
fpapillon283 <72813764+fpapillon283@users.noreply.github.com>
2020-10-13 22:28:41 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+basic.forever(
+basic.showString("Bonjour!")
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `))
+
+//basic.forever(function () {
+  //  basic.showIcon(IconNames.Heart)
+//    basic.pause(500)
+  //  basic.showIcon(IconNames.SmallHeart)
+//    basic.pause(500)
+//})
